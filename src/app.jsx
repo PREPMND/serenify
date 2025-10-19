0eaf1016412b63c6e8bb49ffd7f145044bc59455
Srijan <219679461+PREPMND@users.noreply.github.com>
2025-10-19 21:26:52 +0530

    adding app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,13 @@
-
+import Header from "./header.jsx"
+import Body from "./body-one.jsx"
+import Alz from "./ques.jsx"
+import Inst from "./inst.jsx"
+export default function App(){
+  return(
+    <>
+    <Header/>
+    <Body/>
+    <Inst/>
+    </>
+  )
+}
