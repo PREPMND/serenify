00591d4d0dc177f2fc5a9d729bdd59bbb7e4bfee
Srijan <219679461+PREPMND@users.noreply.github.com>
2025-10-21 07:38:52 +0530

    added closing co mponent

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,14 @@ import Header from "./header.jsx"
 import Body from "./body-one.jsx"
 import Alz from "./ques.jsx"
 import Inst from "./inst.jsx"
+import Closing from "./closing.jsx"
 export default function App(){
   return(
     <>
     <Header/>
     <Body/>
     <Inst/>
+    <Closing/>
     </>
   )
 }
