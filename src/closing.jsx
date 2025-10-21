84382f4ebb0ec082bb505bf5c0e63ef351cba569
Srijan <219679461+PREPMND@users.noreply.github.com>
2025-10-21 07:39:36 +0530

    Add Closing component with support details
    Created a new component for the closing section with support information.

---
+++ src/closing.jsx
@@ -0,0 +1,23 @@
+import nxtfeat from './assets/dys-back.jpg'
+export default function Closing(){
+    return(
+        <>
+        <div className="next-feature">
+            <h3>Coming Soon 🎨</h3>
+            <p>Your personalized calming visuals will appear here.</p>
+            <p>We’re also working on gentle sound therapy to enhance the experience.</p>
+        </div>
+        <hr/>
+        <div className="help-section">
+            <h4>Need Support?</h4>
+            <p>If you or someone you know NEEDS HELP , reach out to:</p>
+        <ul>
+            <li>📞 National Mental Health Helpline: 1800-599-0019</li>
+            <li>✉️ <a href="mailto:support@serenify.ai">support@serenify.ai</a></li>
+        </ul>
+        </div>
+
+        </>
+
+    )
+}
