292ee04e302b83148a0682772bb0c010b98bb174
Srijan <219679461+PREPMND@users.noreply.github.com>
2025-10-23 11:48:08 +0530

    Update dsx.jsx

--- src/dsx.jsx
+++ src/dsx.jsx
@@ -1,28 +1,35 @@
-export default function Dys (){
-    function startAI(formData){
-        const nm=formData.get("nme")
-        alert("Starting "+nm);
+import ReactDOM from "react-dom/client";
+import Results from "./results.jsx"
+export default function Dys() {
+    function getstart(e) {
+        e.preventDefault();
+        const newWindow = window.open("", "_blank");
+        const div = newWindow.document.createElement("div");
+        newWindow.document.body.appendChild(div);
+        newWindow.document.body.style.margin = "0";
+        newWindow.document.body.style.background = "linear-gradient(180deg, #fff6fa,rgb(121, 26, 203))";
+        ReactDOM.createRoot(div).render(<Results />);
     }
-    return(
+    return (
         <>
-        <h3 className="fill">Fill The Desired Details</h3>
-        <form className="ques" action={startAI}>
-            <label className="label">Name:</label>
-            <input className="name" name="nme" type="text" placeholder="Name" required/>
-            <label className="label">Age:</label>
-            <input className="age"  type="number" placeholder="Age" required/>
-            <label className="label">Email:</label>
-            <input className="email" type="email" placeholder="Email" required></input>
-            <label className="label">Like Doing In Free Time:</label>
-            <input className="hobbies" type="text" placeholder=".ie music,drawing" required></input>
-            <label className="label">Would You Like Visuals That Motivate You:</label>
-            <input className="moti-ys" type="text" placeholder="Yes Or No" required></input>
-            <label className="label">Favorite Scenery:</label>
-            <input className="scenery" type="text" placeholder=".ie sky,forest,beach,open fields" required></input>
-            <label className="label">Preferred Relaxation Time:</label>
-            <input className="time" type="text" placeholder=".ie morning,evening,night,afternoon" required></input>
-            <button className="smb">Start The Therapy!</button>
-        </form>
+            <h3 className="fill">Fill The Desired Details</h3>
+            <form className="ques" onSubmit={getstart}>
+                <label className="label">Name:</label>
+                <input className="name" name="nme" type="text" placeholder="Name" required />
+                <label className="label">Age:</label>
+                <input className="age" type="number" placeholder="Age" required />
+                <label className="label">Email:</label>
+                <input className="email" type="email" placeholder="Email" required />
+                <label className="label">Like Doing In Free Time:</label>
+                <input className="hobbies" type="text" placeholder=".ie music,drawing" required />
+                <label className="label">Would You Like Visuals That Motivate You:</label>
+                <input className="moti-ys" type="text" placeholder="Yes Or No" required />
+                <label className="label">Favorite Scenery:</label>
+                <input className="scenery" type="text" placeholder=".ie sky,forest,beach,open fields" required></input>
+                <label className="label">Preferred Relaxation Time:</label>
+                <input className="time" type="text" placeholder=".ie morning,evening,night,afternoon" required></input>
+                <button className="smb">Start The Therapy!</button>
+            </form>
         </>
     )
 }
